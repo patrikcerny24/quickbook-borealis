d8c5b73e80fa66dd63fd9bae5708454c9328167c
mtjchm <matejcham@gmail.com>
2025-09-23 13:32:45 +0200

    Revert "added deployment workflow"
    This reverts commit 822f7926e24f726b0f6cf35ade85629412c94957.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
-};
+const nextConfig = {};
 
 export default nextConfig;
